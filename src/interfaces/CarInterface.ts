70d617b21a13de0205cc4643b6a126cc992da0cc
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-11 18:54:41 -0300

    feat: zodifica interface Car

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,6 +1,12 @@
-import { Vehicle } from './VehicleInterface';
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
 
-export interface Car extends Vehicle {
-  doorsQty: number;
-  seatsQty: number;
-}
+// https://github.com/colinhacks/zod#extend
+const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
+});
+
+type Car = z.infer<typeof CarSchema>;
+
+export { CarSchema, Car };
