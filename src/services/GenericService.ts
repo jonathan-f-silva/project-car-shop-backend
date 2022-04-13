c6be3774f7bcbd3e08ab0f532630e4fb6e54dcf5
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-13 19:12:44 -0300

    ✨ feat: `GenericService` inicial

---
+++ src/services/GenericService.ts
@@ -0,0 +1,18 @@
+import { Model } from '../interfaces/ModelInterface';
+import { Service } from '../interfaces/ServiceInterface';
+
+export default class GenericService<T> implements Service<T> {
+  constructor(private model: Model<T>) { }
+
+  create = async (item: T): Promise<T> => this.model.create(item);
+
+  read = async (): Promise<T[]> => this.model.read();
+
+  readOne = async (id: string): Promise<T | null> => this.model.readOne(id);
+
+  update = async (id: string, item: T): Promise<T | null> =>
+    this.model.update(id, item);
+
+  delete = async (id: string): Promise<T | null> =>
+    this.model.delete(id);
+}
