92ea602ec1a2ff27c740f34404f4aa9c956a6b13
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-18 00:27:13 -0300

    ✨ feat: `GenericRouter` e `server`

---
+++ src/routes/GenericRouter.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import GenericController from '../controllers/GenericController';
+
+class GenericRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: GenericController<T>,
+    route: string = controller.route,
+  ) {
+    this.router.get(route, controller.read);
+    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(route, controller.create);
+    this.router.put(route, controller.update);
+    this.router.delete(route, controller.delete);
+  }
+}
+
+export default GenericRouter;
