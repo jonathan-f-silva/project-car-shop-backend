c052758becc95e4a9e09b70ec9118439c5391ae9
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-21 18:22:59 -0300

    ♻️🧪 refactor(tests): `MongoModel`

--- src/tests/unit/models/MongoModel.test.ts
+++ src/tests/unit/models/MongoModel.test.ts
@@ -1,152 +1,112 @@
 import { expect } from 'chai';
-import Sinon from 'sinon';
-import { testMock, testMockResult, TestMongoModel } from '../../mocks/TestMocks';
+import sinon from 'sinon';
+import MongoModel from '../../../models/MongoModel';
 
 describe('MongoModel', () => {
-  const testModel = new TestMongoModel() as any;
-    
+  beforeEach(sinon.restore);
+
+  const testModel = new MongoModel('', {});
+
   describe('#create()', () => {
-    before(() => {
-      Sinon.stub(testModel.model, 'create').resolves(testMockResult);
+    it('gera erro caso a DB gere erro', async () => {
+      sinon.stub(testModel.dao, 'create').rejects();
+      try {
+        await testModel.create({});
+        expect('não gerou erro').to.be.false;
+      }
+      catch (error) { expect(error).to.be.an('Error') }
     });
-  
-    after(() => {
-      Sinon.restore();
-    })
 
-    it('cria um novo documento com os dados passados e id', async () => {
-      const test = await testModel.create(testMock);
-
-      expect(testModel.model.create.calledWith(testMock)).to.be.true;
-      expect(test).to.be.deep.equal(testMockResult);
+    it('retorna o documento criado', async () => {
+      sinon.stub(testModel.dao, 'create').resolves({});
+      const test = await testModel.create({});
+      expect(test).to.be.deep.equal({});
     });
   });
 
   describe('#read()', () => {
-    before(() => {
-      Sinon.stub(testModel.model, 'find').resolves([testMockResult]);
+    it('gera erro caso a DB gere erro', async () => {
+      sinon.stub(testModel.dao, 'find').rejects();
+      try {
+        await testModel.read();
+        expect('não gerou erro').to.be.false;
+      }
+      catch (error) { expect(error).to.be.an('Error') }
     });
-  
-    after(() => {
-      Sinon.restore();
-    })
 
     it('retorna um array com todos os documentos', async () => {
+      sinon.stub(testModel.dao, 'find').resolves([]);
       const test = await testModel.read();
-
-      expect(test).to.be.deep.equal([testMockResult]);
+      expect(test).to.be.deep.equal([]);
     });
   });
 
   describe('#readOne()', () => {
-    describe('ao passar um id de documento existente', () => {
-      before(() => {
-        Sinon.stub(testModel.model, 'findOne').resolves(testMockResult);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
-
-      it('retorna o documento referenciado pela id', async () => {
-        const { _id } = testMockResult;
-        const test = await testModel.readOne(_id);
-
-        expect(testModel.model.findOne.calledWith({ _id })).to.be.true;
-        expect(test).to.be.deep.equal(testMockResult);
-      });
+    it('gera erro caso a DB gere erro', async () => {
+      sinon.stub(testModel.dao, 'findOne').rejects();
+      try {
+        await testModel.readOne('1');
+        expect('não gerou erro').to.be.false;
+      }
+      catch (error) { expect(error).to.be.an('Error') }
     });
 
-    describe('ao passar um id de documento inexistente', () => {
-      before(() => {
-        Sinon.stub(testModel.model, 'findOne').resolves(null);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
+    it('retorna null caso a DB não tenha o documento', async () => {
+      sinon.stub(testModel.dao, 'findOne').resolves(null);
+      const test = await testModel.readOne('1');
+      expect(test).to.be.equal(null);
+    });
 
-      it('retorna null', async () => {
-        const test = await testModel.readOne(99999);
-
-        expect(testModel.model.findOne.calledWith({ _id: 99999 })).to.be.true;
-        expect(test).to.be.deep.equal(null);
-      });
+    it('retorna o documento na DB', async () => {
+      sinon.stub(testModel.dao, 'findOne').resolves({} as any);
+      const test = await testModel.readOne('1');
+      expect(test).to.be.deep.equal({});
     });
   });
 
   describe('#update()', () => {
-    describe('ao passar um id de documento existente', () => {
-      const updatedMockResult = { ...testMockResult, name: 'newName' };
-      
-      before(() => {
-        Sinon.stub(testModel.model, 'findByIdAndUpdate').resolves(updatedMockResult);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
-
-      it('retorna o documento atualizado referenciado pela id', async () => {
-        const { _id } = testMockResult;
-        const test = await testModel.update(_id, updatedMockResult);
-
-        expect(test).to.be.deep.equal(updatedMockResult);
-      });
+    it('gera erro caso a DB gere erro', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndUpdate').rejects();
+      try {
+        await testModel.update('1', {});
+        expect('não gerou erro').to.be.false;
+      }
+      catch (error) { expect(error).to.be.an('Error') }
     });
 
-    describe('ao passar um id de documento inexistente', () => {
-      const updatedMockResult = { ...testMockResult, _id: 99999, name: 'newName' };
-      
-      before(() => {
-        Sinon.stub(testModel.model, 'findByIdAndUpdate').resolves(null);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
+    it('retorna null caso a DB não tenha o documento', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndUpdate').resolves(null);
+      const test = await testModel.update('1', {});
+      expect(test).to.be.equal(null);
+    });
 
-      it('retorna null', async () => {
-        const { _id } = updatedMockResult;
-        const test = await testModel.update(_id, updatedMockResult);
-
-        expect(test).to.be.deep.equal(null);
-      });
+    it('retorna o documento atualizado da DB', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndUpdate').resolves({} as any);
+      const test = await testModel.update('1', {});
+      expect(test).to.be.deep.equal({});
     });
   });
 
   describe('#delete()', () => {
-    describe('ao passar um id de documento existente', () => {
-      before(() => {
-        Sinon.stub(testModel.model, 'findByIdAndDelete').resolves(testMockResult);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
-
-      it('retorna o documento removido referenciado pela id', async () => {
-        const { _id } = testMockResult;
-        const test = await testModel.delete(_id);
-
-        expect(test).to.be.deep.equal(testMockResult);
-      });
+    it('gera erro caso a DB gere erro', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndDelete').rejects();
+      try {
+        await testModel.delete('1');
+        expect('não gerou erro').to.be.false;
+      }
+      catch (error) { expect(error).to.be.an('Error') }
     });
 
-    describe('ao passar um id de documento inexistente', () => {
-      before(() => {
-        Sinon.stub(testModel.model, 'findByIdAndDelete').resolves(null);
-      });
-    
-      after(() => {
-        Sinon.restore();
-      })
+    it('retorna null caso a DB não tenha o documento', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndDelete').resolves(null);
+      const test = await testModel.delete('1');
+      expect(test).to.be.equal(null);
+    });
 
-      it('retorna null', async () => {
-        const test = await testModel.delete(99999);
-
-        expect(test).to.be.deep.equal(null);
-      });
+    it('retorna o documento removido da DB', async () => {
+      sinon.stub(testModel.dao, 'findByIdAndDelete').resolves({} as any);
+      const test = await testModel.delete('1');
+      expect(test).to.be.deep.equal({});
     });
   });
 });
