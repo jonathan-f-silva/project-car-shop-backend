c27834e49f0cd2fe281fc307db8f721532cb9aaf
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-21 15:06:05 -0300

    ♻️ refactor: `MongoModel` genérico

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,12 +1,20 @@
+import { Schema } from 'mongoose';
 import { z } from 'zod';
 import { VehicleSchema } from './VehicleInterface';
 
+export type Car = z.infer<typeof CarZodSchema>;
+
+export const CarMongoSchema = new Schema({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+}, { versionKey: false });
+
 // https://github.com/colinhacks/zod#extend
-const CarSchema = VehicleSchema.extend({
+export const CarZodSchema = VehicleSchema.extend({
   doorsQty: z.number().int().min(2).max(4),
   seatsQty: z.number().int().min(2).max(7),
 });
-
-type Car = z.infer<typeof CarSchema>;
-
-export { CarSchema, Car };
--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,10 +1,29 @@
-// código baseado em https://app.betrybe.com/course/back-end/mongodb-com-nodejs-e-poo/mongodb-e-poo/91006798-2877-4004-9cf5-d2d72a859272/conteudos/3a09b443-b999-49fe-bcd8-ca2863db53f3/models/13be5bfe-6c23-41f8-a4b1-3d5acfdf1c70?use_case=side_bar
+// código inspirado em https://app.betrybe.com/course/back-end/mongodb-com-nodejs-e-poo/mongodb-e-poo/91006798-2877-4004-9cf5-d2d72a859272/conteudos/3a09b443-b999-49fe-bcd8-ca2863db53f3/models/13be5bfe-6c23-41f8-a4b1-3d5acfdf1c70?use_case=side_bar
 
-import { Model as M, Document } from 'mongoose';
+// Como resolver o problema de tipagem do schema do mongoose
+// https://github.com/Automattic/mongoose/issues/10046#issuecomment-811194721
+
+import {
+  Model as M,
+  model as createModel,
+  Schema,
+  SchemaDefinition,
+  SchemaDefinitionType,
+} from 'mongoose';
 import { Model } from '../interfaces/ModelInterface';
 
-abstract class MongoModel<T> implements Model<T> {
-  constructor(protected model: M<T & Document>) { }
+export default class MongoModel<T> implements Model<T> {
+  protected schema: Schema<T>;
+
+  protected model: M<T>;
+
+  constructor(
+    mongoCollectionName: string,
+    mongoSchemaDefinition: SchemaDefinition<SchemaDefinitionType<T>>,
+  ) {
+    this.schema = new Schema<T>(mongoSchemaDefinition);
+    this.model = createModel<T>(mongoCollectionName, this.schema);
+  }
 
   create = async (obj: T): Promise<T> => this.model.create(obj);
 
@@ -19,5 +38,3 @@ abstract class MongoModel<T> implements Model<T> {
   delete = async (id: string): Promise<T | null> =>
     this.model.findByIdAndDelete(id);
 }
-
-export default MongoModel;
