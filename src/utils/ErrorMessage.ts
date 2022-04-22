2204d81df21069f799439d5569da04167470a21f
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-21 22:01:58 -0300

    ♻️ refactor: `ErrorMiddleware` & friends

---
+++ src/utils/ErrorMessage.ts
@@ -0,0 +1,7 @@
+enum ErrorMessage {
+  OBJECT_NOT_FOUND = 'Object not found',
+  VALIDATION_ERROR = 'Data is invalid',
+  SERVER_ERROR = 'Oops! Something wrong is not right!',
+}
+
+export default ErrorMessage;
