95c65b5ef8b45480dee7622e51fb07fe23a1fea0
Jonathan Ferreira <j0n4t@hotmail.com>
2022-04-21 22:07:11 -0300

    🐛fix: ajuste no carregamento do `ErrorMiddleware`

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express, { Router } from 'express';
-import 'express-async-errors';
 
 import connectToDatabase from './connection';
-import { errorMiddleware } from './utils/error';
 
 class App {
   public app: express.Application;
@@ -14,7 +12,6 @@ class App {
 
   public startServer(PORT: string | number = 3001): void {
     connectToDatabase().then(() => {
-      this.app.use(errorMiddleware);
       this.app.listen(
         PORT,
         () => console.log(`Server running here 👉 http://localhost:${PORT}`),
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import 'express-async-errors';
+
 import App from './app';
 import makeCarRouter from './factories/carRouterFactory';
+import ErrorMiddleware from './utils/ErrorMiddleware';
 
 const server = new App();
 
 server.addRouter(makeCarRouter());
+server.app.use(ErrorMiddleware);
 
 export default server;
